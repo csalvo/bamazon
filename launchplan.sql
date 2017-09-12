870e737342305b936c1b16e5484ae6b8c223e0f5
Cristina Salvo <csalvo@spendsetter.com>
2017-09-12 14:03:04 -0400

    sql files

---
+++ launchplan.sql
@@ -0,0 +1,12 @@
+INSERT INTO products (`product_name`, `department_name`, `price`, `stock_quantity`) 
+VALUES ("Pants", "Womens", 13, 9), 
+("Sweatshirt", "Mens", 44, 3), 
+("Headphones", "Electronics", 52, 20), 
+("Earrings", "Jewelry", 6, 4), 
+("Picture Frame", "Houseware", 12, 19),
+("Cool T-Shirt", "Womens", 6, 10),
+("Cupcakes", "Snacks", 5, 2),
+("Fluffy Blanket", "Houseware", 20, 7),
+("Potato", "Produce", 1, 100),
+("Blender", "Kitchen", 90, 3)
+LIMIT 10;
