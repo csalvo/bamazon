bec72d15d36211dc5b7e5f2654d4f8779b299469
Cristina Salvo <csalvo@spendsetter.com>
2017-09-14 13:52:31 -0400

    view total sales supervisor

---
+++ bamazonSupervisor.js
@@ -0,0 +1,49 @@
+var consoleTable = require('console.table');
+var inquirer = require('inquirer');
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazon"
+});
+
+
+connection.connect(function(err) {
+    if (err) throw err;
+    getSupervisorInput();
+});
+
+var getSupervisorInput = function(){
+inquirer.prompt([
+{
+	type: "list",
+	name: "supervisorAction",
+	message: "What would you like to do?",
+	choices: ["View Product Sales by Department", "Create New Department"]
+}
+]).then(function(response){
+	if (response.supervisorAction === "View Product Sales by Department"){
+		viewSales();
+	} else if (response.supervisorAction === "Create New Department"){
+		createNewDepartment();
+	}
+});
+}
+
+var viewSales = function(){
+	connection.query("SELECT d.department_id, d.department_name, d.overhead_cost, SUM(p.product_sales) as total_product_sales, (SUM(p.product_sales) - d.overhead_cost) AS total_profit FROM departments d LEFT JOIN products p ON d.department_name = p.department_name GROUP BY d.department_id, d.department_name, d.overhead_cost;", function(err,res){
+										console.table(res);
+									});
+connection.end();
+}
+
+var createNewDepartment = function(){
+
+}
