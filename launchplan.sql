5c90769767e11672ba4791809ecb4ec003e63a46
Cristina Salvo <csalvo@spendsetter.com>
2017-09-14 13:36:18 -0400

    updated sql for part 3

--- launchplan.sql
+++ launchplan.sql
@@ -1,3 +1,4 @@
+--part one
 INSERT INTO products (`product_name`, `department_name`, `price`, `stock_quantity`) 
 VALUES ("Pants", "Womens", 13, 9), 
 ("Sweatshirt", "Mens", 44, 3), 
@@ -10,3 +11,14 @@ VALUES ("Pants", "Womens", 13, 9),
 ("Potato", "Produce", 1, 100),
 ("Blender", "Kitchen", 90, 3)
 LIMIT 10;
+
+--part three
+INSERT INTO departments (`department_name`, `overhead_cost`)
+VALUES ("Womens", 1000),
+("Mens", 1000),
+("Electronics", 2300),
+("Jewelry", 1340),
+("Houseware", 900),
+("Snacks", 400),
+("Produce", 650),
+("Kitchen",3400);
--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+--part one 
 DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 
@@ -10,4 +11,15 @@ CREATE TABLE products(
   price INT default 0,
   stock_quantity INT default 0,
   PRIMARY KEY (id)
+);
+
+--part three
+ALTER TABLE `bamazon`.`products` 
+ADD COLUMN `product_sales` DECIMAL(10,5) NULL AFTER `stock_quantity`;
+
+
+CREATE TABLE departments(
+	department_id INT NOT NULL AUTO_INCREMENT,
+	department_name VARCHAR(100) NOT NULL,
+	overhead_cost DECIMAL(10,5) NOT NULL default 0
 );
